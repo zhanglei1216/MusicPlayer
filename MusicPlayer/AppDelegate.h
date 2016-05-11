47c4eeef9dabf940b69794e19a872a3ec2d90ba7
zhanglei <326023094@qq.com>
2016-05-11 10:15:43 +0800

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by 张磊 on 16/5/11.
+//  Copyright © 2016年 张磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by 张磊 on 16/5/11.
+//  Copyright © 2016年 张磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
